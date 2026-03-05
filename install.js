09aee3349a76d369e09cd57968be662c16660bac
Anchorite <ai_anchorite@proton.me>
2026-03-05 17:13:27 +1300

    Update install.js

--- install.js
+++ install.js
@@ -1,36 +1,27 @@
 module.exports = {
   run: [
-    // Edit this step to customize the git repository to use
-/*     {
-      method: "shell.run",
-      params: {
-        message: [
-          "git clone https://github.com/jordandare/echo-tts.git app",
-        ]
-      }
-    }, */
-    // Edit this step with your custom install commands
+
     {
       method: "shell.run",
       params: {
-        venv: "env",                // Edit this to customize the venv folder path
-        path: "app",                // Edit this to customize the path to start the shell from
+        venv: "env",
+        path: "app",
         message: [
           "uv pip install -r requirements.txt"
         ]
       }
     },
-    // Delete this step if your project does not use torch
+
     {
       method: "script.start",
       params: {
         uri: "torch.js",
         params: {
-          venv: "env",                // Edit this to customize the venv folder path
-          path: "app",                // Edit this to customize the path to start the shell from
-          flashattention: true,   // uncomment this line if your project requires flashattention
-          // xformers: true   // uncomment this line if your project requires xformers
-          triton: true   // uncomment this line if your project requires triton
+          venv: "env",
+          path: "app",
+          flashattention: true,
+          // xformers: true,   // uncomment this line if your project requires xformers
+          triton: true,
           // sageattention: true   // uncomment this line if your project requires sageattention
         }
       }
